b2c96ccc5a4e639586a5b263c38f18ead89579bb
miEquipo <brasanluc123@gmail.com>
2023-04-20 20:32:59 -0300

    usandoComandosDeSql

--- FT-M4-master/02-sql/homework/01 - Exercises/homework.js
+++ FT-M4-master/02-sql/homework/01 - Exercises/homework.js
@@ -1,18 +1,18 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = "select *  from  movies where duration < 90 ;";
+const ejercicio02 = "select *  from  movies where duration < 90 ";
 
-const ejercicio03 = "select * from movies where year between 1930 and 1940;";
+const ejercicio03 = "select * from movies where year between 1930 and 1940";
 
-const ejercicio04 = "select * from movies where title like ('%til%');";
+const ejercicio04 = "select * from movies where title like ('%til%')";
 
-const ejercicio05 = "select * from movies where cardinality(actors)=1;";
+const ejercicio05 = "select * from movies where cardinality(actors)=1";
 
 const ejercicio06 =
-  "select title , avg(rating) from movies, unnest(ratings)rating group by title;";
+  "select title , avg(rating) from movies, unnest(ratings)rating group by title";
 
 const ejercicio07 =
-  "SELECT * FROM movies WHERE title LIKE '%Fast and%' AND year = 2016;";
+  "SELECT * FROM movies WHERE title LIKE '%Fast and%' AND year = 2016";
 
 module.exports = {
   ejercicio02,
