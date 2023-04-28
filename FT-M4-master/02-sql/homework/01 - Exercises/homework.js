fbc96d5c18c3660b1cf2aebaf78e6b30a18556d3
miEquipo <brasanluc123@gmail.com>
2023-04-27 23:04:29 -0300

    guardandoDeNuevo

--- FT-M4-master/02-sql/homework/01 - Exercises/homework.js
+++ FT-M4-master/02-sql/homework/01 - Exercises/homework.js
@@ -2,11 +2,11 @@
 
 const ejercicio02 = "SELECT * FROM    movies where duration < 90 ";
 
-const ejercicio03 = "SELECT * FROM  movies where year between 1930 and 1940";
+const ejercicio03 = "SELECT * FROM  MOVIES where year between 1930 and 1940";
 
-const ejercicio04 = "SELECT * FROM  movies where title like ('%til%')";
+const ejercicio04 = "SELECT * FROM  MOVIES where title like ('%til%')";
 
-const ejercicio05 = "SELECT * FROM  movies where cardinality(actors)=1";
+const ejercicio05 = "SELECT * FROM  MOVIES where cardinality(actors)=1";
 
 const ejercicio06 =
   "SELECT TITLE, AVG(rating) from movies, unnest(ratings)rating group by title";
