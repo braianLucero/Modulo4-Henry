feff8ff9e0ba9e46202650cdb210a2c976126835
miEquipo <brasanluc123@gmail.com>
2023-05-12 09:48:08 -0300

    guardandoDormida

--- FT-M4-master/02-sql/homework/01 - Exercises/homework.js
+++ FT-M4-master/02-sql/homework/01 - Exercises/homework.js
@@ -4,7 +4,7 @@ const ejercicio02 = "SELECT * FROM   MOVIES where duration < 90 ";
 
 const ejercicio03 = "SELECT * FROM  MOVIES where YEAR between 1930 and 1940";
 
-const ejercicio04 = "SELECT * FROM  MOVIES where title LIKE ('%til%')";
+const ejercicio04 = "SELECT * FROM  MOVIES where  TITLE LIKE ('%til%')";
 
 const ejercicio05 = "SELECT * FROM  MOVIES WHERE CARDINALITY(actors)=1";
 
